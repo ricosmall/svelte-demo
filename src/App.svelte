13c4a8bf5c8655388baaa4b48a2f64385bdfdfdf
ricosmall <805163443@qq.com>
2019-09-05 19:10:28 +0800

    feat: finish first version

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,87 @@
-<script></script>
+<script>
+  import Search from "./Search.svelte";
+  import SearchResults from "./SearchResults.svelte";
+  import LoadingIndicator from "./LoadingIndicator.svelte";
+  import { onMount } from "svelte";
+
+  const UNSPLASH_ACCESS_KEY = "your access key";
+
+  let searchQuery = "";
+  let searchTerm = null;
+  let totalPages = null;
+  let searchResults = [];
+  let nextPage = 1;
+  let isLoading = false;
+  let observer;
+  let target;
+
+  const options = {
+    rootMargin: "0px 0px 300px",
+    threshold: 0
+  };
+
+  const loadMoreResults = entries => {
+    entries.forEach(entry => {
+      if (nextPage === 1 || isLoading) return;
+      if (entry.isIntersecting) {
+        searchUnsplash();
+      }
+    });
+  };
+
+  onMount(() => {
+    observer = new IntersectionObserver(loadMoreResults, options);
+    target = document.querySelector(".loading-indicator");
+
+    observer.observe(target);
+  });
+
+  function handleSubmit() {
+    searchTerm = searchQuery.trim();
+    searchResults = [];
+    totalPages = null;
+    nextPage = 1;
+
+    if (!searchTerm) return;
+
+    searchUnsplash();
+  }
+
+  function searchUnsplash() {
+    isLoading = true;
+
+    const endpoint = `https://api.unsplash.com/search/photos?query=${searchTerm}&page=${nextPage}&per_page=28&client_id=${UNSPLASH_ACCESS_KEY}`;
+
+    fetch(endpoint)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.total === 0) {
+          alert("No Photos were found for your search query.");
+          return;
+        }
+
+        searchResults = [...searchResults, ...data.results];
+        totalPages = data.total_pages;
+
+        if (nextPage < totalPages) {
+          nextPage += 1;
+        }
+      })
+      .catch(() => alert("An error occured!"))
+      .finally(() => {
+        isLoading = false;
+
+        if (nextPage >= Number(totalPages)) {
+          observer.unobserve(target);
+        }
+      });
+  }
+</script>
 
 <style>
   .App {
@@ -18,4 +101,14 @@
 
 <main class="App">
   <h1>Unsplash Search App</h1>
+
+  <Search bind:query={searchQuery} {handleSubmit} />
+
+  <SearchResults results={searchResults} />
+
+  <div class="loading-indicator">
+    {#if isLoading}
+      <LoadingIndicator />
+    {/if}
+  </div>
 </main>
---
+++ src/Search.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let query;
+  export let handleSubmit;
+</script>
+
+<style>
+  .search-input {
+    width: 100%;
+    max-width: 800px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+    padding: 10px 20px;
+    font-size: 20px;
+    margin-bottom: 50px;
+  }
+</style>
+
+<div class="search">
+  <form class="search-form" on:submit|preventDefault={handleSubmit}>
+    <input
+      type="search"
+      class="search-input"
+      placeholder="Search Unsplash's library of over 1 million photos"
+      bind:value={query} />
+  </form>
+</div>
